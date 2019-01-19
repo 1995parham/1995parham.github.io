9e915d0d8a86e9cd62c103a49a9a20f8ee2bc97a
Parham Alvani <parham.alvani@gmail.com>
2019-01-20 01:04:21 +0330

    Registers routing module and sections components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AboutComponent } from './sections/about/about.component';
+import { EducationComponent } from './sections/education/education.component';
+import { ExperienceComponent } from './sections/experience/experience.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    EducationComponent,
+    ExperienceComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
