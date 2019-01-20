abdaaae21b2a23519d4154b8e1a0c2110a9fdcb1
Parham Alvani <parham.alvani@gmail.com>
2019-01-20 07:54:14 +0330

    Implements divider component.

---
+++ src/app/divider/divider.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-divider',
+  templateUrl: './divider.component.html',
+  styleUrls: ['./divider.component.less']
+})
+export class DividerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
