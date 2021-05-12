99c633b32cf605104351e4ade594a2e78ce10c19
Parham Alvani <parham.alvani@gmail.com>
2021-05-13 00:50:06 +0430

    feat: Add Divider and Title Components

---
+++ components/divider.tsx
@@ -0,0 +1,26 @@
+function Divier() {
+  return (
+    <div className="divider">
+      <div data-svg-drawing="yes" className="zigzag large clearfix ">
+        <svg
+          xmlSpace="preserve"
+          viewBox="0 0 69.172 14.975"
+          width="37"
+          height="28"
+          y="0px"
+          x="0px"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+          xmlns="http://www.w3.org/2000/svg"
+          version="1.1"
+        >
+          <path
+            d="M1.357,12.26 10.807,2.81 20.328,12.332 29.781,2.879 39.223,12.321 48.754,2.79 58.286,12.321 67.815,2.793 "
+            style={{ strokeDasharray: "93.9851, 93.9851", strokeDashoffset: 0 }}
+          ></path>
+        </svg>
+      </div>
+    </div>
+  );
+}
+
+export default Divier;
