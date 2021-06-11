f2b0280ca6994ad4d1ec3648701fdd44bc1951b4
Parham Alvani <parham.alvani@gmail.com>
2021-06-11 08:54:54 +0430

    feat: Improve Page Template

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ import { AppProps } from "next/app";
 
 function MainApp({ Component, pageProps }: AppProps) {
   return (
-    <div id="wrapper" className="margLTop margLBottom">
+    <>
       <Head>
         <title>Parham Alvani</title>
         <link
@@ -17,103 +17,105 @@ function MainApp({ Component, pageProps }: AppProps) {
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🐼</text></svg>"
         />
       </Head>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-3 left-content">
-            <header id="header">
-              <div className="main-header">
-                <figure className="img-profile">
-                  <img src="/me-1.png" alt="Parham Alvani" />
-                  <figcaption className="name-profile">
-                    <span>PARHAM ALVANI</span>
-                  </figcaption>
-                </figure>
-                <nav id="main-nav" className="main-nav clearfix tabbed">
-                  <ul>
-                    <li className="nav-item">
-                      <a href="/" className="nav-link">
-                        <i className="fas fa-user"></i>About Me
-                      </a>
-                    </li>
-                    <li className="nav-item">
-                      <a href="/education" className="nav-link">
-                        <i className="fas fa-graduation-cap"></i>Education
-                      </a>
-                    </li>
-                    <li className="nav-item">
-                      <a href="/experience" className="nav-link">
-                        <i className="fas fa-briefcase"></i>Work
-                      </a>
-                    </li>
-                    <li className="nav-item">
-                      <a href="/blog" className="nav-link">
-                        <i className="fas fa-blog"></i>Blog
-                      </a>
-                    </li>
-                    <li className="nav-item">
-                      <a href="/contact" className="nav-link">
-                        <i className="fas fa-pen"></i>Contact
-                      </a>
-                    </li>
-                    <li className="nav-item">
-                      <a href="/lecture" className="nav-link">
-                        <i className="fas fa-book"></i>Lecture
-                      </a>
-                    </li>
-                    <li className="nav-item">
+      <div id="wrapper" className="margLTop margLBottom">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-3 left-content">
+              <header id="header">
+                <div className="main-header">
+                  <figure className="img-profile">
+                    <img src="/me-1.png" alt="Parham Alvani" />
+                    <figcaption className="name-profile">
+                      <span>PARHAM ALVANI</span>
+                    </figcaption>
+                  </figure>
+                  <nav id="main-nav" className="main-nav clearfix tabbed">
+                    <ul>
+                      <li className="nav-item">
+                        <a href="/" className="nav-link">
+                          <i className="fas fa-user"></i>About Me
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a href="/education" className="nav-link">
+                          <i className="fas fa-graduation-cap"></i>Education
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a href="/experience" className="nav-link">
+                          <i className="fas fa-briefcase"></i>Work
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a href="/blog" className="nav-link">
+                          <i className="fas fa-blog"></i>Blog
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a href="/contact" className="nav-link">
+                          <i className="fas fa-pen"></i>Contact
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a href="/lecture" className="nav-link">
+                          <i className="fas fa-book"></i>Lecture
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a
+                          href="https://1995parham.github.io/stopwatch/"
+                          rel="noreferrer"
+                          target="_blank"
+                          className="nav-link"
+                        >
+                          <i className="fas fa-stopwatch"></i>Stopwatch
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a
+                          href="https://1995parham.github.io/MeInBSc/"
+                          rel="noreferrer"
+                          target="_blank"
+                          className="nav-link"
+                        >
+                          <i className="fas fa-memory"></i>Me in B.Sc.
+                        </a>
+                      </li>
+                    </ul>
+                  </nav>
+                </div>
+                <div className="bottom-header bgWhite ofsTSmall ofsBSmall tCenter">
+                  <ul className="social">
+                    <li className="anim">
                       <a
-                        href="https://1995parham.github.io/stopwatch/"
-                        rel="noreferrer"
+                        href="https://github.com/1995parham"
                         target="_blank"
-                        className="nav-link"
+                        rel="noreferrer"
                       >
-                        <i className="fas fa-stopwatch"></i>Stopwatch
+                        <i className="fab fa-github"></i>
                       </a>
                     </li>
-                    <li className="nav-item">
+                    <li className="anim">
                       <a
-                        href="https://1995parham.github.io/MeInBSc/"
-                        rel="noreferrer"
+                        href="http://stackoverflow.com/users/4242097/parham-alvani"
                         target="_blank"
-                        className="nav-link"
+                        rel="noreferrer"
                       >
-                        <i className="fas fa-memory"></i>Me in B.Sc.
+                        <i className="fab fa-stack-overflow"></i>
                       </a>
                     </li>
                   </ul>
-                </nav>
-              </div>
-              <div className="bottom-header bgWhite ofsTSmall ofsBSmall tCenter">
-                <ul className="social">
-                  <li className="anim">
-                    <a
-                      href="https://github.com/1995parham"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      <i className="fab fa-github"></i>
-                    </a>
-                  </li>
-                  <li className="anim">
-                    <a
-                      href="http://stackoverflow.com/users/4242097/parham-alvani"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      <i className="fab fa-stack-overflow"></i>
-                    </a>
-                  </li>
-                </ul>
-                <p>I Have a Dream</p>
-              </div>
-            </header>
-          </div>
-          <div className="col-md-9 right-content">
-            <Component {...pageProps} />
+                  <p>I Have a Dream</p>
+                </div>
+              </header>
+            </div>
+            <div className="col-md-9 right-content">
+              <Component {...pageProps} />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
