3f8501bf5e2344b3643cecf051f97845ab999818
Parham Alvani <parham.alvani@gmail.com>
2024-01-21 21:40:44 +0000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,11 +21,11 @@ export default function Home({ resumeTag }: HomeProps) {
             <div className="row">
               <div className="col-md-8">
                 <p>
-                  I was born on February 20, 1995, in Tehran, where I currently live.
+                  I was born on February 20, 1995, in Tehran.
                   I work as a Backend Engineer and I love to write codes.
                   Everything for me began in 2012 when I finished my high school
                   and start my bachelor at Amirkabir University in Software engineer.
-                  My first programming course was with Prof. Bakhshi which was great
+                  My first programming course was with <a href="https://ceit.aut.ac.ir/~bakhshis/">Prof. Bakhshi</a> which was great
                   and motivated me to learn more about Unix, Linux and the world of Computer.
                 </p>
                 <br />
